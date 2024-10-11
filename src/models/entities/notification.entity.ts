cc3fde4fe6b0adba78e6a3b3fafd6fb22cee1987
Ægir <0x5ea000000@gmail.com>
2024-10-11 22:52:45 +0700

    feat: add timestamp to notification

--- src/models/entities/notification.entity.ts
+++ src/models/entities/notification.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity } from 'typeorm';
 import { BaseEntity } from '@shared/base/models/base.entity';
 import { NotificationStatus } from '@modules/notification/types/notification';
+import dayjs from 'dayjs';
 
 @Entity({ name: 'notification' })
 export class NotificationEntity extends BaseEntity {
@@ -12,6 +13,9 @@ export class NotificationEntity extends BaseEntity {
 
     @Column({ type: 'varchar' })
     public type: string;
+
+    @Column({ type: 'int', default: dayjs().unix() })
+    timestamp: number;
 
     @Column({ type: 'enum', enum: NotificationStatus, default: NotificationStatus.Unread })
     public status: NotificationStatus;
