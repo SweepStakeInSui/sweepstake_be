09d394f7a472555c758589b8b122c6e48385e944
Draply <doduc0610@gmail.com>
2024-11-18 16:36:48 +0700

    chore: refactor constant

--- src/modules/chain/constants/target.constant.ts
+++ src/modules/chain/constants/target.constant.ts
@@ -1,3 +1,9 @@
 export const TransactionTarget = {
-    Deposit: '',
+    Deposit: 'deposit',
+    Withdraw: 'withdraw',
+    CreateMarket: 'create_market',
+    ExecuteOrder: 'execute_order',
+    ClaimReward: 'claim_reward',
+    SweepStake: 'sweepstake',
+    Market: 'conditional_market',
 };
